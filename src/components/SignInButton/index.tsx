66eb76e8377af8a3cbc759f1b3c47bcfef66111f
jorgelucasalima <jorgelucasalima@gmail.com>
2022-01-12 21:12:45 -0300

    criado botão de signin do github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,14 +1,30 @@
-import {FaGithub} from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
 
+
 export function SignInButton () {
-  return (
+
+  //criei essa variavel para verificar se o usuário ta logado
+  // deixando icone do github verde se tiver e amarelo se não tiver
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
     <button 
       type="button"
       className={styles.botaoSingIn}
     >
-      <FaGithub/>
+      <FaGithub color="#04d361"/>
+      Jorge Lucas Lima
+      <FiX className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button 
+      type="button"
+      className={styles.botaoSingIn}
+    >
+      <FaGithub color="#eba417"/>
       Sing in with Github
     </button>
-  ) 
+  )
 }
