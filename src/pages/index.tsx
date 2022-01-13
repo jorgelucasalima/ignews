f249af504aa7eac629e5e4110a3fb8906859c86e
jorgelucasalima <jorgelucasalima@gmail.com>
2022-01-13 16:15:29 -0300

    integrado api do stripe e incluindo preço no index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
+import {GetServerSideProps} from 'next'
 import Head from 'next/head'
 import styles from './home.module.scss'
 
 import { SubscribeButton } from '../components/SubscribeButton'
+import {stripe} from '../services/stripe'
 
-export default function Home() {
+
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -16,9 +28,9 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         
@@ -27,4 +39,24 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  
+  const price = await stripe.prices.retrieve('price_1K1aZYBFOT9k9TbPTdCSGNju')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+  }).format(price.unit_amount / 100)
+  }
+  
+  return {  
+    props: {
+      product,  
+    }
+  }
 }
