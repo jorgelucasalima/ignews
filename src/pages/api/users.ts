1bbb835351263e7536f5f436edb605f9608a0402
jorgelucasalima <jorgelucasalima@gmail.com>
2022-01-14 15:14:49 -0300

    incluido API routes do next

---
+++ src/pages/api/users.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+
+  const users = [
+    {id: 1, name: 'John Doe'},
+    {id: 2, name: 'Jane Doe'},
+    {id: 3, name: 'Jan Doe'},
+    {id: 4, name: 'ane Doe'},
+
+  ]
+
+  return response.json(users)
+
+
+}
