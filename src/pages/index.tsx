93b4a897e27a26bf57d79f0bf017449db6dbc333
jorgelucasalima <jorgelucasalima@gmail.com>
2022-01-14 14:53:48 -0300

    incluido revalidate para gerar pagina em 24horas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,6 +57,7 @@ export const getStaticProps: GetStaticProps = async () => {
   return {  
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //a cada 24 horas um novo html é gerado
   }
 }
