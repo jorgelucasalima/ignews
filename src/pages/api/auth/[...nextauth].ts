45372e231ce9e187021803521f1de45f7d154e75
jorgelucasalima <jorgelucasalima@gmail.com>
2022-01-17 21:01:15 -0300

    criado integração com faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { fauna } from "../../../services/fauna"
+import { query } from "faunadb"
+
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -8,5 +12,37 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user',
     }),
-  ]
+  ],
+
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+
+  callbacks: {
+    async signIn({user, account, profile}): Promise<boolean> {
+      
+      const {email} = user
+      
+      try {
+
+        await fauna.query(
+          query.Create(
+            query.Collection('users'),
+            { data: {email}}
+          )
+        )
+      
+        return true
+        
+      } catch (error) {
+        
+        return false
+
+      }
+
+      
+    
+    }
+  }
+
 })
