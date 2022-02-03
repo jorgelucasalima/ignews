c4c95ee229fc15de74986f5c5735aec3cf33e724
jorgelucasalima <jorgelucasalima@gmail.com>
2022-02-03 15:29:26 -0300

    atualização no stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 
@@ -8,10 +9,27 @@ interface SubscribeButtonProps {
 
 
 export function SubscribeButton( {priceId}: SubscribeButtonProps ) {
+  
+
+  const {session} = useSession();
+
+  function handleSubscribe() {
+
+      if (!session) {
+        signIn('github')
+        return
+      }
+
+      //criação da checkout session
+
+      
+  }
+  
   return (
     <button
       type="button"
       className={styles.botaoSubscribe}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
