b96df356da096d04d8ed3ac4ed5d7e3c44f066fe
jorgelucasalima <jorgelucasalima@gmail.com>
2022-02-09 14:53:40 -0300

    estilizado pagina de posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,3 +1,33 @@
-export default function Posts() {
+import  Head  from 'next/head';
+import styles from './styles.module.scss';
 
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="">
+            <time>12 de março de 1994</time>
+            <strong>Criando post</strong>
+            <p>monark tomou no cu</p>
+          </a>
+          <a href="">
+            <time>12 de março de 1994</time>
+            <strong>Criando post</strong>
+            <p>monark tomou no cu</p>
+          </a>
+          <a href="">
+            <time>12 de março de 1994</time>
+            <strong>Criando post</strong>
+            <p>monark tomou no cu</p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
 }
